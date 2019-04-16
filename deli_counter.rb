e92d26f16d4524ad377ff3d73412333492008f52
Alison Golden <alisong779@yahoo.com>
2019-04-16 22:13:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+ def line(katz_deli)
+  arr = []
+  if katz_deli.empty? == true 
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |person, index| arr << "#{index+1}. #{person}"
+  end
+  puts "The line is currently: #{arr.join(" ")}"
+  end
+end 
+
+def take_a_number(katz_deli, name)
+  line = []
+  if katz_deli.empty? == true 
+    katz_deli.push(name)
+  else 
+    katz_deli.each_with_index do |person, index|
+      puts "Welcome, #{person}. You are number #{index+1} in line." 
+end
+end 
+end 
